8c8947a9b2dcc46f3d4ceefa1f05dd62165e0977
bartoszkopecc <56592329+bartoszkopecc@users.noreply.github.com>
2019-10-20 12:45:23 +0200

    Book C# - Jacek Matulewski

---
+++ Notatnik.NET/Notatnik.NET/Font.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BartoszKopec.WpfUtils
+{
+    public struct Font
+    {
+        public FontFamily Family { get; set; }
+        public string FamilyName
+        {
+            get
+            {
+                return Family.ToString();
+            }
+        }
+
+        public FontStyle Style { get; set; }
+        public FontWeight Weight { get; set; }
+        public textDecorationCollection TextDecorations { get; set; }
+        public double Size { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Brush Brush { get { return new SolidColorBrush(Color); } }
+
+        public static Font Default
+        {
+            get
+            {
+                return new Font()
+                {
+                    Family = new FontFamily("Segoe UI"),
+                    Style = FontStyles.Normal,
+                    Weight = FontWeights.Normal,
+                    TextDecorations = null,
+                    Size = 12,
+                    Color = Colors.Black
+                };
+            }
+        }
+
+        public static System.Drawing.Font ToSystemDrawingFont(Font font)
+        {
+            System.Drawing.FontStyle style = (font.Style == FontStyles.Italic) ? System.Drawing.FontStyle.Italic : System.Drawing.FontStyle.Regular;
+            if (font.Weight == FontWeights.Bold) style |= System.Drawing.FontStyle.Bold;
+            if (font.TextDecorations.Contain(System.Windows.TextDecorations.Underline[0]))
+                style |= System.Drawing.FontStyle.Underline;
+            if (font.TextDecorations.Contains(System.Windows.TextDecorations.Strikethrough[0]))
+                style |= System.Drawing.FontStyle.Strikeout;
+            System.Drawing.Font _font = new System.Drawing.Font(font.FamilyName, (int)font.Size, style);
+            return _font;
+        }
+
+        public static Font FromSystmDrawingFont(System.Drawing.Font sdFont, System.Drawing.Color sdColor)
+        {
+            Font font = new Font();
+            font.Family = new FontFamily(sdFont.FontFamily.Name);
+            font.Style = sdFont.Italic ? FontStyles.Italic : FontStyles.Normal;
+            font.Weight = sdFont.Bold ? FontWeight.Bold : FontWeights.Regular;
+            font.TextDecorations = new textDecorationCollection();
+            if (sdFont.Underline)
+                font.TextDecorations.Add(System.Windows.TextDecorations.Underline);
+            if (sdFont.Strikeout)
+                font.TextDecorations.Add(System.Windows.TextDecorations.Strikethrough);
+            font.Size = sdFont.Size;
+            font.Color = sdColor.Convert();
+            return font;
+        }
+
+        public void ApplyTo(Control control)
+        {
+            control.FontFamily = Family;
+            control.FontStyle = Style;
+            control.FontWeight = Weight;
+            control.FontSize = Size;
+            control.Foreground = Brush;
+        }
+
+        public void ApplyTo(TextBox textBox)
+        {
+            ApplyTo(textBox as Control);
+            textBox.TextDecorations = TextDecorations;
+        }
+
+        public void ApplyTo(TextBlock textBlock)
+        {
+            textBlock.FontFamily = Family;
+            textBlock.FontStyle = Style;
+            textBlock.FontWeight = Weight;
+            textBlock.TextDecorations = TextDecoration;
+            textBlock.FontSize = Size;
+            textBlock.Foreground = Brush;
+        }
+    }
+}
---
+++ Notatnik.NET/Notatnik.NET/MainWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media;
+using BartoszKopec.WpfUtils;
+
+namespace Notatnik.NET
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        
+
+        private string ścierzkaPliku = null;
+        private bool czyTekstZmieniony = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Wybierz plik tekstowy";
+            openFileDialog.DefaultExt = "txt";
+            openFileDialog.Filter = "Pliki tekstowe (*.txt|*.txt|Pliki XML (*.xml|xml|Pliki źródłowe (*.cs)|*.cs|Wszystkie pliki (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+
+            saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Zapisz plik tekstowy";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.Filter = openFileDialog.Filter;
+            saveFileDialog.FilterIndex = 1;
+            
+        }
+
+        private void MenuItem_Otwórz_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(ścierzkaPliku))
+            {
+                openFileDialog.InitialDirectory = Path.GetDirectoryName(ścierzkaPliku);
+                openFileDialog.FileName = Path.GetFileName(ścierzkaPliku);
+            }
+
+            bool? wynik = openFileDialog.ShowDialog();
+            if(wynik.HasValue && wynik.Value)
+            {
+                ścierzkaPliku = openFileDialog.FileName;
+                textBox.Text = File.ReadAllText(ścierzkaPliku);
+                statusBarText.Text = Path.GetFileName(ścierzkaPliku);
+                czyTekstZmieniony = false;
+            }
+        }
+
+        private void MenuItem_ZapiszJako_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(ścierzkaPliku))
+            {
+                saveFileDialog.InitialDirectory = Path.GetDirectoryName(ścierzkaPliku);
+                saveFileDialog.FileName = Path.GetFileName(ścierzkaPliku);
+
+            }
+
+            bool? wynik = saveFileDialog.ShowDialog();
+            if(wynik.HasValue && wynik.Value)
+            {
+                ścierzkaPliku = saveFileDialog.FileName;
+                File.WriteAllText(ścierzkaPliku, textBox.Text);
+                statusBarText.Text = Path.GetFileName(ścierzkaPliku);
+                czyTekstZmieniony = false;
+            }
+        }
+
+        private void MenuItem_Zapisz_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(ścierzkaPliku))
+            {
+                File.WriteAllText(ścierzkaPliku, textBox.Text);
+                czyTekstZmieniony = false;
+            }
+            else MenuItem_ZapiszJako_Click(sender, e);
+        }
+
+        private void MenuItem_Zamknij_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            czyTekstZmieniony = true;
+        }
+
+        private void zapytajOZapisanietekstuDoPliku(object sender, out bool anuluj)
+        {
+            anuluj = false;
+            if (czyTekstZmieniony)
+            {
+                MessageBoxResult wynik =
+                    MessageBox.Show("Czy zapisać zmiany w edytowanym dokumencie?",
+                                    this.Title,
+                                    MessageBoxButton.YesNoCancel,
+                                    MessageBoxImage.Question,
+                                    MessageBoxResult.Cancel);
+                switch (wynik)
+                {
+                    case MessageBoxResult.Yes:
+                        MenuItem_Zapisz_Click(sender, null);
+                        break;
+                    case MessageBoxResult.No: break;
+                    case MessageBoxResult.Cancel:
+                    default:
+                        anuluj = true;
+                        break;
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            bool anuluj;
+            zapytajOZapisanietekstuDoPliku(sender, out anuluj);
+            e.Cancel = anuluj;
+
+        }
+
+        private void MenuItem_Nowy_Click(object sender, RoutedEventArgs e)
+        {
+            bool anuluj;
+            zapytajOZapisanietekstuDoPliku(sender, out anuluj);
+            if (!anuluj) textBox.Text = "";
+        }
+
+        private void MenuItem_Cofnij_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Undo();
+        }
+        private void MenuItem_Powtórz_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Redo();
+        }
+        private void MenuItem_Wytnij_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Cut();
+        }
+        private void MenuItem_Kopiuj_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Copy();
+        }
+
+        private void MenuItem_Wklej_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Paste();
+        }
+
+        private void MenuItem_Usuń_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.SelectedText="";
+        }
+
+        private void MenuItem_ZaznaczWszystko_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.SelectAll();
+        }
+
+        private void MenuItem_GodzinaData_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.SelectedText = System.DateTime.Now.ToString();
+        }
+
+        private void MenuItem_ZawijanieWierszy_Click(object sender, RoutedEventArgs e)
+        {
+            bool czyPozycjaZaznaczona = (sender as MenuItem).IsChecked;
+            textBox.TextWrapping = czyPozycjaZaznaczona ? TextWrapping.Wrap : TextWrapping.NoWrap;
+        }
+
+        private void MenuItem_PasekNarzędzi_Click(object sender, RoutedEventArgs e)
+        {
+            bool czyPozycjaZaznaczona = (sender as MenuItem).IsChecked;
+            toolBar.Visibility = czyPozycjaZaznaczona ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void MenuItem_PasekStanu_Click(object sender, RoutedEventArgs e)
+        {
+            bool czyPozycjaZaznaczona = (sender as MenuItem).IsChecked;
+            statusBar.Visibility = czyPozycjaZaznaczona ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void MenuItem_KolorTła_Click(object sender, RoutedEventArgs e)
+        {
+            Color kolorTła = Colors.White;
+            if (textBox.Background is SolidColorBrush)
+                kolorTła = (textBox.Background as SolidColorBrush).Color;
+            if (WindowsFormsHelper.ChooseColor(ref kolorTła))
+                textBox.Background = new SolidColorBrush(kolorTła);
+        }
+    }
+}
