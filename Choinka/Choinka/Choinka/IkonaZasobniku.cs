f53f73a3df9fa481bd3cfefc45977a171f43ffd1
bartoszkopecc <56592329+bartoszkopecc@users.noreply.github.com>
2019-10-20 12:42:18 +0200

    Book C# - Jacek Matulewski

---
+++ Choinka/Choinka/Choinka/IkonaZasobniku.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Choinka
+{
+    public class IkonaZasobniku
+    {
+        private NotifyIcon notifyIcon;
+        private System.Windows.Window okno;
+
+        public IkonaZasobniku(System.Windows.Window okno)
+        {
+            //ikona
+            string nazwaIkony = "christmas.ico";
+            string nazwaAplikacji = Application.ProductName;
+            System.Windows.Resources.StreamResourceInfo sri = System.Windows.Application.GetResourceStream(new Uri(@"/" + nazwaAplikacji + ";component/" + nazwaIkony, UriKind.RelativeOrAbsolute));
+            Icon icon = new Icon(sri.Stream);
+
+            //menu
+            ContextMenuStrip menu = tworzMenu();
+
+            //ikona w zasobniku
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = icon;
+            notifyIcon.Text = "Choinka" + DateTime.Now.Year.ToString();
+            notifyIcon.ContextMenuStrip = menu;
+            notifyIcon.Visible = true;
+
+            notifyIcon.DoubleClick += (s, e) =>
+              {
+                  int ileDniDoŚwiąt = (new DateTime(DateTime.Today.Year, 12, 24) - DateTime.Now).Days;
+                  notifyIcon.BalloonTipTitle = notifyIcon.Text;
+                  notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+                  notifyIcon.BalloonTipText = "Do świąt pozostało " + ileDniDoŚwiąt + " dni";
+                  notifyIcon.ShowBalloonTip(3000);
+              };
+
+            //menu aplikacji
+            this.okno = okno;
+            okno.MouseRightButtonDown += (s, e) =>
+              {
+                  System.Windows.Point p = okno.PointToScreen(e.GetPosition(okno));
+                  menu.Show((int)p.X, (int)p.Y);
+              };
+        }
+
+        public bool Widoczny
+        {
+            get
+            {
+                return notifyIcon.Visible;
+            }
+
+            set
+            {
+                notifyIcon.Visible = value;
+            }
+        }
+
+        private ContextMenuStrip tworzMenu()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+
+            ToolStripMenuItem ukryjToolStripMenuItem = new ToolStripMenuItem("Ukryj");
+            ukryjToolStripMenuItem.Click += (s, e) => { okno.Hide(); };
+            menu.Items.Add(ukryjToolStripMenuItem);
+
+            ToolStripMenuItem przywrocToolStripMenuItem = new ToolStripMenuItem("Przywróć");
+            przywrocToolStripMenuItem.Click += (s, e) => { okno.Show(); };
+            menu.Items.Add(przywrocToolStripMenuItem);
+
+            ToolStripMenuItem zamknijToolStripMenuItem = new ToolStripMenuItem("Zamknij");
+            zamknijToolStripMenuItem.Click += (s, e) => { okno.Close(); };
+            menu.Items.Add(zamknijToolStripMenuItem);
+
+            menu.Items.Add(new ToolStripSeparator());
+
+            ToolStripMenuItem oAutorzeToolStripMenuItem = new ToolStripMenuItem("O...");
+            oAutorzeToolStripMenuItem.Click += (s, e) =>
+              {
+                  System.Windows.SplashScreen splashScreen = new System.Windows.SplashScreen("SplashScreen.png");
+                  splashScreen.Show(false, true);
+                  System.Threading.Thread.Sleep(1000);
+                  splashScreen.Close(new TimeSpan(0, 0, 1));
+              };
+            menu.Items.Add(oAutorzeToolStripMenuItem);
+
+            return menu;
+        }
+
+        public void Usuń()
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            notifyIcon = null;
+        }
+    }
+}
---
+++ Choinka/Choinka/Choinka/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace Choinka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IkonaZasobniku ikonaZasobniku;
+        MediaPlayer odtwarzaczAudio;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ikonaZasobniku = new IkonaZasobniku(this);
+
+            string scierzkaPlikuDzwiekowego = System.IO.Path.GetFullPath("DJANGO.mp3");
+            if(System.IO.File.Exists(scierzkaPlikuDzwiekowego))
+            {
+                odtwarzaczAudio = new MediaPlayer();
+                odtwarzaczAudio.Open(new Uri(scierzkaPlikuDzwiekowego, UriKind.RelativeOrAbsolute));
+                odtwarzaczAudio.MediaEnded += (s, e) => { odtwarzaczAudio = null; };
+                odtwarzaczAudio.Play();
+            }
+
+        }
+
+        #region Przenoszenie okna
+        private bool czyPrzenoszenie = false;
+        private Point punktPoczątkowy;
+        private Cursor kursor;
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ButtonState==Mouse.LeftButton)
+            {
+                czyPrzenoszenie = true;
+                kursor = Cursor;
+                Cursor = Cursors.Hand;
+                punktPoczątkowy = e.GetPosition(this);
+            }
+
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(czyPrzenoszenie)
+            {
+                Vector przesuniecie = e.GetPosition(this) - punktPoczątkowy;
+                Left += przesuniecie.X;
+                Top += przesuniecie.Y;
+            }
+        }
+
+        private void Window_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(czyPrzenoszenie)
+            {
+                Cursor = kursor;
+                czyPrzenoszenie = false;
+            }
+        }
+        #endregion
+
+        #region Zamykanie okna
+        private void Window_PerviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) this.Close();
+        }
+
+        bool zakonczonaAnimacjaZnikania = false;
+               
+        private void Okno_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(!zakonczonaAnimacjaZnikania)
+            {
+                Storyboard scenorysZnikaniaOkna = this.Resources["scenorysZnikaniaOkna"] as Storyboard;
+                scenorysZnikaniaOkna.Begin();
+                e.Cancel = true;
+            }
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            zakonczonaAnimacjaZnikania = true;
+            Close();
+        }
+        #endregion
+    }
+}
