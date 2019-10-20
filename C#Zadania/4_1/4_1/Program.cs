4d18cf774af1852f8b04975d0f587f8b780feba0
bartoszkopecc <56592329+bartoszkopecc@users.noreply.github.com>
2019-10-20 12:34:53 +0200

    Book Mirosław Kubiak - C# Zadania z programowania

---
+++ C#Zadania/4_1/4_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10, i;
+
+            int[] dane = new int[n];
+
+            Console.WriteLine("Program wyświetla zawartość tablicy jednowymiarowej " + n + " elementowej");
+            Console.WriteLine();
+
+            for(i=0; i<n; i++)
+            {
+                dane[i] = i;
+                Console.WriteLine("dane[" + i + "] = " + dane[i]);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ C#Zadania/4_2/4_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10, i;
+            int[] dane = new int[n];
+            Console.WriteLine("Program wyświetla tablicę jednowymiarową " + n + " elementową");
+            Console.WriteLine();
+
+            for(i=0;i<n;i++)
+            {
+                dane[i] = n - 1 - i;
+                Console.WriteLine("dane[" + i + "] = " + dane[i]);
+
+            }
+            Console.Read();
+
+        }
+    }
+}
